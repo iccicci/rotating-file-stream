14854bc01f10b697b0de91241658f3ef31873f6d
Daniele Ricci <daniele.ricci@buongiorno.com>
2015-09-15 12:37:41 +0200

    node 0.10 test fix

--- test/02write.js
+++ test/02write.js
@@ -84,13 +84,24 @@ describe("write", function() {
 			assert.equal(this.rfs.ev.rotated.length, 0);
 		});
 
-		it("1 single write", function() {
-			assert.equal(this.rfs.ev.single, 1);
-		});
+		if(process.version.match(/^v0.10/)) {
+			it("3 single write", function() {
+				assert.equal(this.rfs.ev.single, 1);
+			});
 
-		it("1 multi write", function() {
-			assert.equal(this.rfs.ev.multi, 1);
-		});
+			it("0 multi write", function() {
+				assert.equal(this.rfs.ev.multi, 1);
+			});
+		}
+		else {
+			it("1 single write", function() {
+				assert.equal(this.rfs.ev.single, 1);
+			});
+
+			it("1 multi write", function() {
+				assert.equal(this.rfs.ev.multi, 1);
+			});
+		}
 
 		it("file content", function() {
 			assert.equal(fs.readFileSync("test.log"), "test\ntest\ntest\ntest\n");
