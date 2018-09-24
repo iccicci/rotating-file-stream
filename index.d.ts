cef7e73847959eae031e4a4ea61422b3d5c5b6f4
Daniele Ricci <daniele.icc@gmail.com>
2018-09-24 21:24:59 +0200

    test fix for #29

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,21 @@
 import { WriteStream } from "fs";
 
 interface RfsOptions {
-	compress?: string | Function | boolean
-	highWaterMark?: number
-	history?: string
-	immutable?: boolean
+	compress?:        string | Function | boolean
+	highWaterMark?:   number
+	history?:         string
+	immutable?:       boolean
 	initialRotation?: boolean
-	interval?: string
-	maxFiles?: number
-	maxSize?: string
-	mode?: number
-	path?: string
-	rotate?: number
-	rotationTime?: boolean
-	size?: string
+	interval?:        string
+	maxFiles?:        number
+	maxSize?:         string
+	mode?:            number
+	path?:            string
+	rotate?:          number
+	rotationTime?:    boolean
+	size?:            string
 }
 
 declare function RotatingFileStream(fileName: string | Function, options: RfsOptions): WriteStream;
-export = RotatingFileStream;
+
+export default RotatingFileStream;
