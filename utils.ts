b75e5dae84caf4bdd47910917dd50f0f8f341792
Daniele Ricci <daniele.icc@gmail.com>
2022-03-14 17:12:52 +0100

    minor chages

--- utils.ts
+++ utils.ts
@@ -1,10 +1,13 @@
-"use strict";
-
-import { unlink, writeFile } from "fs";
+import { rm, writeFile } from "fs/promises";
 
 const common: string[] = ["*gz", "*log", "*tmp", "*txt", ".gitignore", ".npmignore", ".nyc_output", "coverage", "node_modules", ""];
 const git: string[] = ["dist"];
 const npm: string[] = [".*", "index.ts", "test", "tsconfig.*", "utils.ts"];
 
-if(process.argv[2] === "clean") unlink("index.js", (): void => {});
-if(process.argv[2] === "ignore") writeFile(".gitignore", git.concat(common).join("\n"), (): void => writeFile(".npmignore", npm.concat(common).join("\n"), (): void => {}));
+if(process.argv[2] === "clean") rm("dist", { force: true, recursive: true });
+if(process.argv[2] === "ignore") {
+  (async () => {
+    await writeFile(".gitignore", git.concat(common).join("\n"));
+    await writeFile(".npmignore", npm.concat(common).join("\n"));
+  })();
+}
